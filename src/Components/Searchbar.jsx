33857bdf5ef65de98682012c6d7f6196b9c95cbd
arjunss25 <arjunsuresh386@gmail.com>
2024-10-06 20:56:18 +0530

    updates

--- src/Components/Searchbar.jsx
+++ src/Components/Searchbar.jsx
@@ -8,7 +8,7 @@ const Searchbar = () => {
     <div className='absolute w-full h-[50vh] bg-white top-0 left-0 z-[50]'>
         <div className="top-sec w-full flex items-center justify-center gap-3 lg:gap-10 h-[15vh]">
             <GoSearch className='text-[1.5rem] cursor-pointer'/>
-            <input type="text" className='w-[70vw] sm:w-[80vw] h-[6vh] rounded-[2rem] bg-zinc-200' />
+            <input type="text" className='w-[70vw] sm:w-[80vw] h-[6vh] rounded-[2rem] bg-zinc-200 px-5 ' />
             <IoCloseOutline  className='text-[2rem] cursor-pointer'/>
             
         </div>
